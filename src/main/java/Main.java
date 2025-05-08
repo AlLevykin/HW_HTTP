283b8e761b2db8112cb0a55639041d3ca52b0952
Левыкин Алексей Александрович <alevykin@alfabank.ru>
2025-05-08 13:45:11 +0500

    Настройка

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,29 +11,26 @@ import org.apache.hc.core5.util.Timeout;
 import java.io.IOException;
 import java.util.List;
 
-public class Main {
+public static ObjectMapper mapper = new ObjectMapper();
 
-    public static ObjectMapper mapper = new ObjectMapper();
+public static void main(String[] args) throws IOException {
+    CloseableHttpClient httpClient = HttpClientBuilder.create()
+            .setDefaultRequestConfig(RequestConfig.custom()
+                    .setConnectTimeout(Timeout.ofSeconds(5000))    // максимальное время ожидание подключения к серверу
+                    .setResponseTimeout(Timeout.ofSeconds(30000)) // максимальное время ожидания получения данных
+                    .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                    .build())
+            .build();
+    HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
 
-    public static void main(String[] args) throws IOException {
-        CloseableHttpClient httpClient = HttpClientBuilder.create()
-                .setDefaultRequestConfig(RequestConfig.custom()
-                        .setConnectTimeout(Timeout.ofSeconds(5000))    // максимальное время ожидание подключения к серверу
-                        .setResponseTimeout(Timeout.ofSeconds(30000)) // максимальное время ожидания получения данных
-                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
-                        .build())
-                .build();
-        HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
+    CloseableHttpResponse response = httpClient.execute(request);
+    List<Fact> facts = mapper.readValue(response.getEntity().getContent(), new TypeReference<>() {
+    });
+    httpClient.close();
 
-        CloseableHttpResponse response = httpClient.execute(request);
-        List<Fact> facts = mapper.readValue(response.getEntity().getContent(), new TypeReference<>() {});
-        httpClient.close();
-
-        facts.stream().
-                filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0).
-                sorted((f1, f2) -> f2.getUpvotes() - f1.getUpvotes()).
-                forEach(System.out::println);
-
-    }
+    facts.stream().
+            filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0).
+            sorted((f1, f2) -> f2.getUpvotes() - f1.getUpvotes()).
+            forEach(System.out::println);
 
 }
